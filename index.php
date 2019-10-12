7105f042bc4353e1162d00f185f0743ba9204e99
hilmi-a <hilmi.a.aziz@gmail.com>
2019-10-12 15:53:56 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="name">Name</label>
+        <input type="text" name="name" value="Enter Name"><br>
+        <label for="address">Address</label>
+        <input type="text" name="Address" value="Enter Address"><br>
+        <input type="button" value="Submit Data">
+    </form>    
+</body>
+</html>
