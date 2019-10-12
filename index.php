7c6f3a78b8e32bfa020ac8e6c6b26c60e9ac0657
hilmi-a <hilmi.a.aziz@gmail.com>
2019-10-12 16:07:06 +0700

    add style

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
     <title>Document</title>
 </head>
+
 <body>
-    <form action="" method="post">
-        <label for="name">Name</label>
-        <input type="text" name="name" value="Enter Name"><br>
-        <label for="address">Address</label>
-        <input type="text" name="Address" value="Enter Address"><br>
-        <input type="button" value="Submit Data">
-    </form>    
+    <div class="container">
+    <div class="row justify-content-center">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" value="Enter Name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="address">Address</label>
+                <input type="text" name="Address" value="Enter Address" class="form-control">
+            </div>
+            <div class="form-group">
+                <input type="button" value="Submit Data " class="btn btn-primary">
+            </div>
+        </form>
+    </div>
+    </div>
+
 </body>
+
 </html>
